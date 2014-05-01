525305a67d5d1e80fecbd6db0ad272ae440a6262
wmf <wmf@us.ibm.com>
2014-05-01 15:16:26 -0500

    play.sh script to create and populate a VM

--- common/vm/play.sh
+++ common/vm/play.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-kvm -net nic -net user -hda play.img -hdb seed.img -m 1G -nographic -redir :2222::22
 
-# in another window:
-# ssh-keygen -f "/power/home/wmf/.ssh/known_hosts" -R [localhost]:2222
-# ssh -p 2222 -i ../id_rsa -o StrictHostKeyChecking=no -o CheckHostIP=no spyre@localhost
-# or you can login at the QEMU console:
-# press enter a few times after It's aliiiiiive
-# when done, press ctrl-a c then system_powerdown
+make ubuntu-13.10-server-cloudimg-amd64-disk1.img seed.img
+
+# create ephemeral overlay qcow image
+# (we probably could have used -snapshot)
+IMG=`mktemp tmpXXX.img`
+qemu-img create -f qcow2 -b ubuntu-13.10-server-cloudimg-amd64-disk1.img $IMG
+
+# start the VM
+kvm -net nic -net user -hda $IMG -hdb seed.img -m 1G -nographic -redir :2222::22 &
+
+# remove the overlay (qemu will keep it open as needed)
+sleep 2
+rm $IMG
+
+# copy a script in (we could use Ansible for this kind of thing, but...)
+rsync -a -e "ssh -p2222 -i../id_rsa -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no -oConnectionAttempts=60" ../../acme-air/vagrant/install-acme-air.sh  spyre@localhost:~
+
+# run the script
+ssh -p2222 -i../id_rsa -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no spyre@localhost ./install-acme-air.sh
+
+# TODO run the benchmark
+
+# shut down the VM
+ssh -p2222 -i../id_rsa -oUserKnownHostsFile=/dev/null -oStrictHostKeyChecking=no spyre@localhost sudo shutdown -h now
