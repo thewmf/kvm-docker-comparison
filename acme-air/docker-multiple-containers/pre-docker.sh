a613c82a023d482cc5444171d000e6e2e018dd51
Juan Rubio <rubioj@us.ibm.com>
2014-05-06 13:16:17 -0500

    Initial version

---
+++ acme-air/docker-multiple-containers/pre-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+############################################################
+# Provisioning commands *before* containers are created
+############################################################
+
+
+# Make sure the package repository is up to date
+#sudo apt-get update
+
