3a2aa450b3063b2ab554f58426fb0e003713e067
Ram Rajamony <rajamony@us.ibm.com>
2014-07-08 03:51:27 -0500

    Support for doing runs

--- Stream/doit.sh
+++ Stream/doit.sh
@@ -5,3 +5,9 @@ numsockets=2
 for ((i=0;i<10;i++)); do ./linux.sh $numsockets; done
 for ((i=0;i<10;i++)); do ./docker.sh $numsockets; done
 for ((i=0;i<10;i++)); do ./vm.sh $numsockets; done
+
+numsockets=1
+
+for ((i=0;i<10;i++)); do ./linux.sh $numsockets; done
+for ((i=0;i<10;i++)); do ./docker.sh $numsockets; done
+for ((i=0;i<10;i++)); do ./vm.sh $numsockets; done
